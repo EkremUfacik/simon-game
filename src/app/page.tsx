298093a9467aa55e05b12f52cd5004e01b16a02a
ekrem <ekremufacik07@gmail.com>
2023-12-14 11:08:56 +0300

    Refactor useEffect dependency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -65,7 +65,7 @@ const Home = () => {
     if (sequence.length > 0) {
       showSequence();
     }
-  }, [sequence.length]);
+  }, [sequence]);
 
   const handleColorClick = (id: number) => {
     const audio = new Audio(sounds[id]);
